e7e01a3e43f835e95f639925c0a51bff4dc045ae
wildlb <wildlb@hotmail.com>
2021-10-23 22:14:45 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*const btnLogin = document.getElementById('btn-submit');
+
+const form = document.querySelector('.form-container');
+
+btnLogin.addEventListener('click', (event)=>{
+    event.preventDefault();
+    form.classList.add ('form-hide')
+});
+*/
